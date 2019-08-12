6c589fd5fb384f161a308e5c6a538d43cdacd2ce
shreyastawre <tawre.shreyas@gmail.com>
2019-08-11 21:38:12 -0400

    Fixed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,44 @@ const path = require('path');
 const SRC_DIR = path.join(__dirname, '/react-client/src');
 const DIST_DIR = path.join(__dirname, '/react-client/dist');
 const webpack = require('webpack');
-
+require('css-modules-require-hook')({
+  generateScopedName: '[path][name]__[local]__[hash:base64:5]',
+  extensions: ['.scss', '.css'],
+  camelCase: true
+});
 module.exports = {
-  entry: `${SRC_DIR}/index.js`,
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
     path: DIST_DIR,
     filename: 'bundle.js',
   },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.css']
+  },
   module : {
     rules : [
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
       {
         test: /\.css$/,
         loader: 'style-loader!css-loader'
@@ -30,8 +59,32 @@ module.exports = {
         query: {
           presets: ['react', 'es2015']
        }
+      },
+      {
+        test: /\.scss$/,
+        include: [
+          path.join(__dirname, 'node_modules/wix-animations'),
+          path.join(__dirname, 'node_modules/wix-style-react'),
+          path.join(__dirname, 'node_modules/bootstrap-sass') // only if you use Grid component
+        ],
+        loaders: [
+          'style-loader',
+          'css-loader?modules&importLoaders=1&camelCase&localIdentName=[name]__[local]___[hash:base64:5]',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /(?<!\.st)\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        loader: 'file-loader'
       }
-    ]
+    ],
   },
   plugins: [
     new webpack.DefinePlugin({
